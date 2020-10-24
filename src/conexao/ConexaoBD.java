c65b9363feeb65bb4089f131982dff1711c592e7
Enzo Costa <enzocosta083@gmail.com>
2020-10-23 22:11:05 -0300

    terceira versao

--- src/conexao/ConexaoBD.java
+++ src/conexao/ConexaoBD.java
@@ -28,7 +28,7 @@ public class ConexaoBD {
 		try {
 			con.close();
 		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, "(n°1) Erro:" + e);
+			JOptionPane.showMessageDialog(null, "(n°3) Erro:" + e);
 		}
 	}
  }
