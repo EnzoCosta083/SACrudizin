3b2ec04b7baad6a4e1e1ddce3c4f6f3fa119489a
Enzo Costa <enzocosta083@gmail.com>
2020-10-23 21:55:16 -0300

    segunda versao

---
+++ src/conexao/ConexaoBD.java
@@ -0,0 +1,34 @@
+package conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.swing.JOptionPane;
+
+public class ConexaoBD {
+
+
+	public static Connection conectar() {
+		Connection con = null;
+
+		final String URL = "jdbc:mysql://localhost:3306/sa01?useTimezone=true&serverTimezone=UTC";
+		final String USER = "root";
+		final String PASS = "!@#Abc123";
+
+		try {
+			con = DriverManager.getConnection(URL, USER, PASS);
+			return con;
+		} catch (Exception e) {
+			return null;
+	}
+
+	}
+
+	public static void desconectar(Connection con) {
+		try {
+			con.close();
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "(n°2) Erro:" + e);
+		}
+	}
+ }
